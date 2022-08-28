7f2b40bbca274a9a3e3397226fca84c42d789138
Scott Block <scottrblock@gmail.com>
2022-08-28 12:23:42 -0400

    wip

--- src/components/sp-body.tsx
+++ src/components/sp-body.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 import styles from "../styles/grid.module.css";
+import * as use from "@tensorflow-models/universal-sentence-encoder";
+
+declare global {
+  interface Window {
+    use: any;
+  }
+}
 
 export const SpBody = () => {
+  React.useEffect(() => {
+    use.load().then((model) => {
+      // Embed an array of sentences.
+      const sentences = ["Hello.", "How are you?"];
+      model.embed(sentences).then((embeddings) => {
+        // `embeddings` is a 2D tensor consisting of the 512-dimensional embeddings for each sentence.
+        // So in this example `embeddings` has the shape [2, 512].
+        embeddings.print(true /* verbose */);
+      });
+    });
+  }, []);
   return (
     <div className={styles.container}>
       <div className={styles.header}>header</div>
