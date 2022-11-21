768dbd3c5965162099f034233e2352258c88e3cf
Scott Block <scottrblock@gmail.com>
2022-11-21 17:03:06 -0500

    Fix height overflow

--- src/components/sp-button.tsx
+++ src/components/sp-button.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from "react";
 import { Button, Dialog, Classes, Icon } from "@blueprintjs/core";
 import { SpBody } from "./sp-body";
+import styles from "../styles/sp-button.module.css";
 
 const SPButton = () => {
   const [modalOpen, setModalOpen] = React.useState(false);
@@ -23,9 +24,9 @@ const SPButton = () => {
         isOpen={modalOpen}
         onClose={closeModal}
         title="similar pages"
-        style={{ width: "95%", maxWidth: "none", paddingBottom: 0, minHeight: "95vh" }}
+        style={{ width: "95%", maxWidth: "none", paddingBottom: 0, minHeight: "90vh" }}
       >
-        <div className={Classes.DIALOG_BODY}>
+        <div className={`${Classes.DIALOG_BODY} ${styles.graphbodywrap} `}>
           <SpBody></SpBody>
         </div>
       </Dialog>
